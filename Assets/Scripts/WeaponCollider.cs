a21ba6f99116cddd6997ae93000b3db5fdb5e38f
ksmirnov <kirill.smirnov@konux.de>
2020-10-02 18:38:26 +0200

    added sound effects

--- Assets/Scripts/WeaponCollider.cs
+++ Assets/Scripts/WeaponCollider.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class WeaponCollider : MonoBehaviour {
 
     [SerializeField] private GameObject hitVFX;
+    [SerializeField] private AudioClip soundEffect;
 
     private void OnTriggerEnter2D(Collider2D anotherObject) {
         GameObject visualEffectObject = Instantiate(hitVFX, transform.position, transform.rotation);
         Destroy(visualEffectObject, 1f);
         Destroy(gameObject);
+        AudioSource.PlayClipAtPoint(soundEffect, Camera.main.transform.position, 1.0f);
 
         WizardModel wizardModel = anotherObject.GetComponentInParent<WizardModel>();
         if (wizardModel && wizardModel.GetLives() > 0 && !wizardModel.isOwned) {
--- Assets/Scripts/WizardFire.cs
+++ Assets/Scripts/WizardFire.cs
@@ -7,10 +7,12 @@ public class WizardFire : MonoBehaviour {
     [SerializeField] private GameObject weaponObject;
     [SerializeField] private Transform mainTransform;
     [SerializeField] private Transform spawnTransform;
+    [SerializeField] private AudioClip soundEffect;
 
     public void Fire() {
         GameObject weapon = Instantiate(weaponObject, spawnTransform.position, spawnTransform.rotation);
         WeaponModel weaponModel = weapon.transform.GetComponent<WeaponModel>();
         weaponModel.xVelocity = mainTransform.localScale.x * weaponModel.horizontalVelocity;
+        AudioSource.PlayClipAtPoint(soundEffect, Camera.main.transform.position, 0.5f);
     }
 }
